55c448b816f0c0dfa821d14f2669cd167953420a
Michael Nissen Thorup Friis <michael.nissen.thorup.friis@lego.com>
2021-05-31 15:25:40 +0200

    now with dropdown

--- github-project-filter.user.js
+++ github-project-filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub projects - Quick filter by user
 // @namespace    https://www.lego.com
-// @version      0.1
+// @version      0.2
 // @description  Allows you to quickly choose a user from the list of assignees and filter issues by them.
 // @author       Michael Nissen Thorup Friis
 // @match        https://github.com/*/*/projects/*
@@ -17,25 +17,43 @@
     function getUsersFromAssignees() {
         let elementsWithAssignees = document.querySelectorAll("[data-card-assignee]");
         let assignees = []
-        let i;
-        for (i = 0; i < elementsWithAssignees.length; i++) {
+        for (let i = 0; i < elementsWithAssignees.length; i++) {
             let assignee = JSON.parse(elementsWithAssignees[i].getAttribute("data-card-assignee"));
-            let x;
-            for (x = 0; x < assignee.length; x++) {
+            for (let x = 0; x < assignee.length; x++) {
                 if(assignees.includes(assignee[x])) {
                    continue;
                 }
                 assignees.push(assignee[x])
             }
         }
-        console.log(assignees, "");
+        addDropdown(assignees);
+    }
+
+    function addDropdown(assignees) {
+        let controlpanel = document.getElementsByClassName("project-header-controls")[0];
+        let selectList = document.createElement("select");
+        selectList.id = "assigneeFiltering";
+        controlpanel.appendChild(selectList);
+
+        //Create and append the options
+        for (let i = 0; i < assignees.length; i++) {
+            let option = document.createElement("option");
+            option.value = assignees[i];
+            option.text = assignees[i];
+            selectList.appendChild(option);
+        }
+
+        selectList.addEventListener('change', (event) => {
+            let currentProject = location.protocol + '//' + location.host + location.pathname
+            window.location.replace(currentProject + "?card_filter_query=assignee%3A" + event.target.value);
+        });
     }
 
     (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
     function check(changes, observer) {
         if(document.querySelector('.project-column')) {
             observer.disconnect();
-            var delayInMilliseconds = 1000; //1 second
+            let delayInMilliseconds = 1000; //1 second
             setTimeout(function() {
                 getUsersFromAssignees();
             }, delayInMilliseconds);
