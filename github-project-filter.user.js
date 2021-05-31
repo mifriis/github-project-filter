f73e23418162fe677a3159d7ceedf7b03cda1847
Kristian Holm Sejersen <690528+kristianholmsejersen@users.noreply.github.com>
2021-05-31 20:36:09 +0200

    const and clean-up

--- github-project-filter.user.js
+++ github-project-filter.user.js
@@ -11,13 +11,11 @@
 (function() {
     'use strict';
 
-    let users = [];
-
     function getUsersFromAssignees() {
-        let elementsWithAssignees = document.querySelectorAll("[data-card-assignee]");
-        let assignees = []
+        const elementsWithAssignees = document.querySelectorAll("[data-card-assignee]");
+        const assignees = []
         for (let i = 0; i < elementsWithAssignees.length; i++) {
-            let assignee = JSON.parse(elementsWithAssignees[i].getAttribute("data-card-assignee"));
+            const assignee = JSON.parse(elementsWithAssignees[i].getAttribute("data-card-assignee"));
             for (let x = 0; x < assignee.length; x++) {
                 if(assignees.includes(assignee[x])) {
                    continue;
@@ -30,21 +28,21 @@
     }
 
     function addDropdown(assignees) {
-        let controlpanel = document.getElementsByClassName("project-header-controls")[0];
-        let selectList = document.createElement("select");
+        const controlpanel = document.getElementsByClassName("project-header-controls")[0];
+        const selectList = document.createElement("select");
         selectList.classList.add("form-control");
         selectList.id = "assigneeFiltering";
         controlpanel.insertBefore(selectList, controlpanel.firstChild);
 
         for (let i = 0; i < assignees.length; i++) {
-            let option = document.createElement("option");
+            const option = document.createElement("option");
             option.value = assignees[i];
             option.text = assignees[i];
             selectList.appendChild(option);
         }
 
         selectList.addEventListener('change', (event) => {
-            let currentProject = location.protocol + '//' + location.host + location.pathname
+            const currentProject = location.protocol + '//' + location.host + location.pathname
 
             //Perhaps this part could be done without loading the entire page again, ideas?
             window.location.replace(currentProject + "?card_filter_query=assignee%3A" + event.target.value);
@@ -56,12 +54,11 @@
     function check(changes, observer) {
         if(document.querySelector('.project-column')) {
             observer.disconnect();
-            let delayInMilliseconds = 1000; //1 second
+            const delayInMilliseconds = 1000; //1 second
             setTimeout(function() {
                 getUsersFromAssignees();
             }, delayInMilliseconds);
 
         }
     }
-
 })();
