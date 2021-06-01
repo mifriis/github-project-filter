fe9f940097ac5d937e1a47e2ac663941b098acf1
Michael Friis <michael.friis@gmail.com>
2021-06-01 13:23:50 +0200

    Milestone support!

--- github-project-filter.user.js
+++ github-project-filter.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         GitHub projects - Quick filter by user
+// @name         GitHub projects - Quick board filters
 // @namespace    michael.friis.userscripts
-// @version      1.4
-// @description  Allows you to quickly choose a user from the list of assignees and filter issues by them.
+// @version      1.5
+// @description  Select boxes that allow filtering on milestones and assignees easily based on what is on the board
 // @author       Michael Nissen Thorup Friis
 // @match        https://github.com/*/*/projects/*
 // @grant        none
@@ -11,7 +11,67 @@
 (function() {
     'use strict';
 
-    function getUsersFromAssignees() {
+    function setupMilestoneFilter() {
+        function addDropdown(milestones) {
+            const controlpanel = document.getElementsByClassName("project-header-controls")[0];
+            const selectList = document.createElement("select");
+            selectList.classList.add("form-control");
+            selectList.id = "assigneeFiltering";
+            controlpanel.insertBefore(selectList, controlpanel.firstChild);
+
+            for (let i = 0; i < milestones.length; i++) {
+                const option = document.createElement("option");
+                option.value = milestones[i];
+                option.text = milestones[i];
+                selectList.appendChild(option);
+            }
+
+            selectList.addEventListener('change', (event) => {
+                const currentProject = location.protocol + '//' + location.host + location.pathname
+
+                //Perhaps this part could be done without loading the entire page again, ideas?
+                window.location.replace(currentProject + "?card_filter_query=milestone%3A\"" + event.target.value + "\"");
+            });
+        }
+
+        const elementsWithMilestones = document.querySelectorAll("[data-card-milestone]");
+        const milestones = []
+        for (let i = 0; i < elementsWithMilestones.length; i++) {
+            const milestone = JSON.parse(elementsWithMilestones[i].getAttribute("data-card-milestone"));
+            for (let x = 0; x < milestone.length; x++) {
+                if(milestones.includes(milestone[x])) {
+                   continue;
+                }
+                milestones.push(milestone[x])
+            }
+        }
+        milestones.sort();
+        addDropdown(milestones);
+    }
+
+    function setupAssigneeFilter() {
+        function addDropdown(assignees) {
+            const controlpanel = document.getElementsByClassName("project-header-controls")[0];
+            const selectList = document.createElement("select");
+            selectList.classList.add("form-control");
+            selectList.id = "assigneeFiltering";
+            controlpanel.insertBefore(selectList, controlpanel.firstChild);
+
+            for (let i = 0; i < assignees.length; i++) {
+                const option = document.createElement("option");
+                option.value = assignees[i];
+                option.text = assignees[i];
+                selectList.appendChild(option);
+            }
+
+            selectList.addEventListener('change', (event) => {
+                const currentProject = location.protocol + '//' + location.host + location.pathname
+
+                //Perhaps this part could be done without loading the entire page again, ideas?
+                window.location.replace(currentProject + "?card_filter_query=assignee%3A" + event.target.value);
+            });
+        }
+
         const elementsWithAssignees = document.querySelectorAll("[data-card-assignee]");
         const assignees = []
         for (let i = 0; i < elementsWithAssignees.length; i++) {
@@ -27,27 +87,7 @@
         addDropdown(assignees);
     }
 
-    function addDropdown(assignees) {
-        const controlpanel = document.getElementsByClassName("project-header-controls")[0];
-        const selectList = document.createElement("select");
-        selectList.classList.add("form-control");
-        selectList.id = "assigneeFiltering";
-        controlpanel.insertBefore(selectList, controlpanel.firstChild);
 
-        for (let i = 0; i < assignees.length; i++) {
-            const option = document.createElement("option");
-            option.value = assignees[i];
-            option.text = assignees[i];
-            selectList.appendChild(option);
-        }
-
-        selectList.addEventListener('change', (event) => {
-            const currentProject = location.protocol + '//' + location.host + location.pathname
-
-            //Perhaps this part could be done without loading the entire page again, ideas?
-            window.location.replace(currentProject + "?card_filter_query=assignee%3A" + event.target.value);
-        });
-    }
 
     //Github loads in each column async. We need to wait until the first column popsup, then wait a little while longer as the rest load. Please help make this better!
     (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
@@ -56,7 +96,8 @@
             observer.disconnect();
             const delayInMilliseconds = 1000; //1 second
             setTimeout(function() {
-                getUsersFromAssignees();
+                setupAssigneeFilter();
+                setupMilestoneFilter();
             }, delayInMilliseconds);
 
         }
